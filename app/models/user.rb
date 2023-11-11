60ecc55f5b5240067d42ec8d9e0723cc2f3d1407
okmon12 <143723986+okmon12@users.noreply.github.com>
2023-11-11 22:12:59 +0900

    ログイン、ログアウト実装

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password,  presence: true, allow_blank: false
+  validates :first_name, presence: true
+  validates :nickname, presence: true
+  validates :birthdate, presence: true
+  validates :last_name, presence: true
+  validates :first_kana, presence: true
+  validates :last_kana, presence: true
+  
+  #attribute :name, :string
+
+  has_many :items
+  has_many :buyers
+end
