371ffe0dfe420d9691ba4847a23377fa15371079
okmon12 <143723986+okmon12@users.noreply.github.com>
2023-11-21 21:51:15 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @items = Item.all
+    @items = Item.order(created_at: :desc)
 
   end
 
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
 
   def create
