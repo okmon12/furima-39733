d3f10e3e0d4b2a8f9865b4e730028f127cff9584
okmon12 <143723986+okmon12@users.noreply.github.com>
2023-11-23 19:38:25 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :redirect_if_not_owner, only: [:edit]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_if_not_owner, only: [:edit]
+ 
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -39,7 +40,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:product_name, :origin_location_id, :shipping_address_form_id, :shipping_fee_id, :category_id,
                                  :price, :image, :product_description, :product_condition_id).merge(user_id: current_user.id)
   end
-  
+
   def redirect_if_not_owner
     item = Item.find(params[:id])
     redirect_to root_path unless user_signed_in? && item.user_id == current_user.id
